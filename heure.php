2168096c48467f98557e28d3a14d6869b1dd3c5b
Kahan N'guessan <kahan.nguessan@etu.univ-lille.fr>
2019-09-21 12:20:31 +0200

    transfert de tous les fichiers

---
+++ heure.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mon super site</title>
+    <link rel="stylesheet"  href="/home/invite/kahan/assets/css/master.css" />
+  </head>
+  <body>
+    <div class="heure">
+      <h1>Il est <?php echo date('G:i:s'); echo dirname(__FILE__); ?></h1>
+    </div>
+  </body>
+</html>
